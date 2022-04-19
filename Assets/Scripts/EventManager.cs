beaf482081d90480a6d3344399b46adbf00e941f
cazsaragih <chairilazmi03@windowslive.com>
2022-04-19 16:55:01 +0700

    Add EventManager class

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+namespace VirTest.Event
+{
+    public class EventManager
+    {
+        public delegate void DelegateEvent<T>(T evt);
+        private static Dictionary<Type, Delegate> delegates = new Dictionary<Type, Delegate>();
+
+        /// <summary>
+        /// Assign/add delegate function into the dictionary.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="del"></param>
+        public static void AddListener<T>(DelegateEvent<T> del)
+        {
+            Delegate tempDelegate;
+            if (delegates.TryGetValue(typeof(T), out tempDelegate))
+            {
+                DelegateEvent<T> method = tempDelegate as DelegateEvent<T>;
+                delegates[typeof(T)] = method += del;
+            }
+            else
+                delegates[typeof(T)] = del;
+        }
+
+        /// <summary>
+        /// Call delegate function accordingly.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="evt"></param>
+        public static void TriggerEvent<T>(T evt)
+        {
+            Delegate tempDelegate;
+            if (delegates.TryGetValue(typeof(T), out tempDelegate))
+            {
+                DelegateEvent<T> del = tempDelegate as DelegateEvent<T>;
+                del(evt);
+            }
+        }
+
+        /// <summary>
+        /// Remove delegate function from the dictionary.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="del"></param>
+        public static void RemoveListener<T>(DelegateEvent<T> del)
+        {
+            Delegate tempDelegate;
+            if (delegates.TryGetValue(typeof(T), out tempDelegate))
+            {
+                DelegateEvent<T> method = tempDelegate as DelegateEvent<T>;
+                method -= del;
+
+                if (method != null)
+                    delegates[typeof(T)] = method;
+                else
+                    delegates.Remove(typeof(T));
+            }
+        }
+
+        public static void RemoveAllListeners()
+        {
+            delegates.Clear();
+        }
+    }
+}
