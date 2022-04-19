89c1efb7e18c003382ba6b540e1aa9681e015dcf
cazsaragih <chairilazmi03@windowslive.com>
2022-04-20 00:03:47 +0700

    Add scrollable layout

---
+++ Assets/Scripts/UI/ItemUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VirTest.UI
+{
+    public class ItemUI : MonoBehaviour
+    {
+        [SerializeField] private Button addBtn;
+        [SerializeField] private Button reorderBtn;
+        [SerializeField] private Button insertBtn;
+
+        private void Awake()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
